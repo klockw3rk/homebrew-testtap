f35bd33b87de81dff7cbb8c31b7914c85edd82c4
klockw3rk <kw@localhost.localdomain>
2023-03-02 16:10:38 -0500

    add formula

---
+++ testscript.rb
@@ -0,0 +1,19 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/klockw3rk/testscript/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "7574687299ba63aaf0def6c7f4cadc4c6c41e1a0b90349cbfffbc1cff07055b8"
+  license ""
+
+
+  def install
+	bin.install "testscript"
+  end
+
+  test do
+
+  end
+end
